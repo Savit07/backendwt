0181acba53ac63aecc7d0b8f671e9d78bd7ce3fe
Savit07 <savitpandita7@gmail.com>
2024-10-23 01:07:38 +0530

    serum institute

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const app = express();
 // MongoDB Connection
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://pass:pass@cluster0.l2bnk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb+srv://pass:pass@cluster0.l2bnk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
